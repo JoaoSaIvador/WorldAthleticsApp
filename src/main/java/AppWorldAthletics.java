2136a3316ade536688dfc0430f937f14d1ad8f10
João Salvador <jpvsalvador05@gmail.com>
2021-05-15 13:44:44 +0100

    --correction--

--- src/main/java/AppWorldAthletics.java
+++ src/main/java/AppWorldAthletics.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 public class AppWorldAthletics extends JFrame{
     private JPanel paginasGerir;
@@ -27,8 +29,6 @@ public class AppWorldAthletics extends JFrame{
     private JButton botaoGerirAtletasMI;
     private JButton botaoGerirProvasMI;
     private JButton botaoTopMedalhadosMI;
-    private JPanel logoArea;
-    private JPanel buttonArea;
     private JPanel adicionarEvento;
     private JPanel editarEvento;
     private JPanel programaEvento;
@@ -46,16 +46,33 @@ public class AppWorldAthletics extends JFrame{
     private JPanel resultadosFinaisProva;
     private JPanel recordesProva;
     private JList list1;
+    private JTextField paísTextField;
+    private JTextField localTextField;
+    private JTextField dataFimTextField;
+    private JTextField dataInícioTextField;
+    private JTextField nomeTextField;
+    private JButton botaoVerPrograma;
+    private JButton botaoImportarEventos;
+    private JButton botaoAdicionarEvento;
+    private JButton botaoRemoverEvento;
+    private JButton botaoEditarEvento;
     private JTextField textField1;
-    private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
+    private JTextField textField2;
+    private JComboBox selecionarProva;
+    private JComboBox selecionarProva2;
+    private JButton botaoAdicionarProva;
+    private JButton botaoRemoverProva;
+    private JButton botaoOk;
+    private JButton botaoCancelar;
+    private JButton botaoOk2;
+    private JButton botaoCancelar2;
+    private JComboBox selecionarProva3;
+    private JComboBox selecionarProva4;
+    private JButton botaoAdicionar2;
+    private JButton botaoRemover2;
 
     private CardLayout cardLayoutGerir;
     private CardLayout cardLayoutNormalPages;
@@ -64,46 +81,56 @@ public class AppWorldAthletics extends JFrame{
     public AppWorldAthletics() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(PainelPrincipal);
-
         pack();
 
         cardLayoutGerir = (CardLayout) (conteudoGerir.getLayout());
         cardLayoutNormalPages = (CardLayout) (PainelPrincipal.getLayout());
 
+        //Menu Lateral
         botaoGerirEventos.addActionListener(this::botaoGerirEventosActionPerformed);
         botaoGerirAtletas.addActionListener(this::botaoGerirAtletasActionPerformed);
         botaoGerirProvas.addActionListener(this::botaoGerirProvasActionPerformed);
         botaoTopMedalhados.addActionListener(this::botaoTopMedalhadosActionPerformed);
         botaoMenuInicial.addActionListener(this::botaoMenuInicialActionPerformed);
 
+        //Menu Inicial
         botaoGerirEventosMI.addActionListener(this::botaoGerirEventosActionPerformed);
         botaoGerirAtletasMI.addActionListener(this::botaoGerirAtletasActionPerformed);
         botaoGerirProvasMI.addActionListener(this::botaoGerirProvasActionPerformed);
         botaoTopMedalhadosMI.addActionListener(this::botaoTopMedalhadosActionPerformed);
 
+        //Gerir Eventos
+        botaoAdicionarEvento.addActionListener(this::botaoAdicionarEventoActionPerformed);
+        botaoEditarEvento.addActionListener(this::botaoEditarEventoActionPerformed);
+        botaoRemoverEvento.addActionListener(this::botaoRemoverEventoActionPerformed);
+        botaoVerPrograma.addActionListener(this::botaoVerProgramaActionPerformed);
+        botaoImportarEventos.addActionListener(this::botaoImportarEventosActionPerformed);
+
         cardLayoutNormalPages.show(PainelPrincipal, "cardMenuPrincipal");
     }
 
+    //------------------------------ MENU LATERAL/INICIAL ------------------------------
+
     private void botaoGerirEventosActionPerformed(ActionEvent e) {
-        cardLayoutNormalPages.show(PainelPrincipal, "cardFuncionalidades");
+        cardLayoutNormalPages.show(PainelPrincipal, "cardGerir");
         cardLayoutGerir.show(conteudoGerir, "cardGerirEventos");
         setElementsBackgroundColor(elementoGerirEventos);
     }
 
     private void botaoGerirAtletasActionPerformed(ActionEvent e) {
-        cardLayoutNormalPages.show(PainelPrincipal, "cardFuncionalidades");
+        cardLayoutNormalPages.show(PainelPrincipal, "cardGerir");
         cardLayoutGerir.show(conteudoGerir, "cardGerirAtletas");
         setElementsBackgroundColor(elementoGerirAtletas);
     }
 
     private void botaoGerirProvasActionPerformed(ActionEvent e) {
-        cardLayoutNormalPages.show(PainelPrincipal, "cardFuncionalidades");
+        cardLayoutNormalPages.show(PainelPrincipal, "cardGerir");
         cardLayoutGerir.show(conteudoGerir, "cardGerirProvas");
         setElementsBackgroundColor(elementoGerirProvas);
     }
 
     private void botaoTopMedalhadosActionPerformed(ActionEvent e) {
-        cardLayoutNormalPages.show(PainelPrincipal, "cardFuncionalidades");
+        cardLayoutNormalPages.show(PainelPrincipal, "cardGerir");
         cardLayoutGerir.show(conteudoGerir, "cardTopMedalhados");
         setElementsBackgroundColor(elementoTopMedalhados);
     }
@@ -111,6 +138,30 @@ public class AppWorldAthletics extends JFrame{
     private void botaoMenuInicialActionPerformed(ActionEvent e) {
         cardLayoutNormalPages.show(PainelPrincipal, "cardMenuPrincipal");
     }
+
+    //------------------------------ GERIR EVENTOS ------------------------------
+
+    private void botaoAdicionarEventoActionPerformed(ActionEvent actionEvent) {
+        cardLayoutNormalPages.show(PainelPrincipal, "cardAdicionarEvento");
+    }
+
+    private void botaoEditarEventoActionPerformed(ActionEvent actionEvent) {
+        cardLayoutNormalPages.show(PainelPrincipal, "cardEditarEvento");
+    }
+
+    private void botaoRemoverEventoActionPerformed(ActionEvent actionEvent) {
+        JOptionPane.showMessageDialog(null, "O evento foi removido com sucesso!");
+    }
+
+    private void botaoVerProgramaActionPerformed(ActionEvent actionEvent) {
+        cardLayoutNormalPages.show(PainelPrincipal, "cardProgramaEvento");
+    }
+
+    private void botaoImportarEventosActionPerformed(ActionEvent actionEvent) {
+        JOptionPane.showMessageDialog(null, "Filler Text");
+    }
+
+    //------------------------------ FUNÇÕES AUXILIARES ------------------------------
 
     private void setElementsBackgroundColor(JPanel elementoHighlight) {
         for (JPanel elemento : elementos) {
